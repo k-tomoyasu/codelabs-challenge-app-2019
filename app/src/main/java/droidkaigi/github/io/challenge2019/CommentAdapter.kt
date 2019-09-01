e0f97bccc72752dc9e8c9827f0843fa6b4441b50
k-tomoyasu <blhicr1303@gmail.com>
2019-09-01 10:36:52 +0900

    add dummy ad

--- app/src/main/java/droidkaigi/github/io/challenge2019/CommentAdapter.kt
+++ app/src/main/java/droidkaigi/github/io/challenge2019/CommentAdapter.kt
@@ -3,24 +3,55 @@ package droidkaigi.github.io.challenge2019
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import droidkaigi.github.io.challenge2019.dummy.Ad
 import droidkaigi.github.io.challenge2019.data.api.response.Item
+import droidkaigi.github.io.challenge2019.databinding.DummyAdBinding
 import droidkaigi.github.io.challenge2019.databinding.ItemCommentBinding
+import java.lang.IllegalArgumentException
 
 
 class CommentAdapter(
     var comments: List<Item?>
-) : RecyclerView.Adapter<CommentAdapter.ViewHolder>() {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     class ViewHolder(val binding: ItemCommentBinding) : RecyclerView.ViewHolder(binding.root)
+    class AdViewHolder(val binding: DummyAdBinding) : RecyclerView.ViewHolder(binding.root)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemCommentBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            ItemViewType.COMMENT.ordinal -> {
+                val binding = ItemCommentBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                ViewHolder(binding)
+            }
+            ItemViewType.AD.ordinal -> {
+                val binding = DummyAdBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                AdViewHolder(binding)
+            }
+            else -> {
+                throw IllegalArgumentException("undeifiend view type $viewType")
+            }
+        }
     }
 
-    override fun getItemCount(): Int = comments.size
+    override fun getItemViewType(position: Int): Int {
+        return if (position  < comments.size) ItemViewType.COMMENT.ordinal else ItemViewType.AD.ordinal
+    }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.item = comments[position]
+    override fun getItemCount(): Int = comments.size + 1
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder) {
+            is ViewHolder -> {
+                holder.binding.item = comments[position]
+            }
+            is AdViewHolder -> {
+                holder.binding.ad = Ad("it is dummy!")
+            }
+        }
+    }
+
+    enum class ItemViewType(type: Int) {
+        COMMENT(0),
+        AD(1)
     }
 }
---
+++ app/src/main/java/droidkaigi/github/io/challenge2019/dummy/Ad.kt
@@ -0,0 +1,3 @@
+package droidkaigi.github.io.challenge2019.dummy
+
+data class Ad(val title: String)
